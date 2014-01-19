2175bffff4428cfd436cf92c037f9b2085109285
Osaboy <osazuwa@gmail.com>
2014-01-19 18:58:04 +0000

    Remove duplicated directory

--- CH5_Mixing_It_Up/FullNameGreeting.rb
+++
@@ -1,9 +0,0 @@
-puts "This program will ask you for your \'First Name', \'Middle Name' and \'Last Name'"
-puts "Enter your First Name then hit Enter"
-FirstName = gets.chomp
-puts "Enter your Middle Name then hit Enter"
-MiddleName = gets.chomp
-puts "Enter your Last Name then hit Enter"
-LastName = gets.chomp
-puts 'Your name is ' + FirstName + ' ' + MiddleName + ' ' + LastName + ' ' + '? What a lovely name!'
-puts 'Pleased to meet you, ' + FirstName + ' ' + MiddleName + ' ' + LastName + ' '  '. :)'
